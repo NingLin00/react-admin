50b4f2516433bc60bbf0d2cd86e872aef82fd5bb
ninglin00 <845893576@qq.com>
2019-06-24 15:57:49 +0800

    在didmount周期函数中使用定时器更新时间，dayjs完成时间美化 完成时间显示功能

--- src/components/right-header/index.jsx
+++ src/components/right-header/index.jsx
@@ -3,6 +3,7 @@
 import React, { Component } from 'react';
 import { Modal } from 'antd';
 import { withRouter } from 'react-router-dom';
+import dayjs from 'dayjs';
 
 import ConmonButton from "../conmon-button";
 import { getItem, removeItem } from '../../until/storage-tool';
@@ -12,11 +13,25 @@ import './index.less'
 
 const { confirm } = Modal;
 
+//RightHeader组件
 class RightHeader extends Component {
+  state = {
+    //初始化时间数据
+    sysTime : Date.now(),
+  }
 
   componentWillMount(){
+    //从localstorage获取用户名
     this.username = getItem().username
   }
+  componentDidMount(){
+    setInterval(() => {
+      this.setState({
+        sysTime: Date.now()
+      })
+    },1000)
+  }
+  //退出登录
   logout = () => {
     confirm({
       title: '您确认要退出登录吗?',
@@ -31,6 +46,7 @@ class RightHeader extends Component {
     })
   }
   render() {
+    const { sysTime } = this.state;
     return <div>
       <div className="header-main-top">
         <span>欢迎, {this.username}</span>
@@ -39,7 +55,7 @@ class RightHeader extends Component {
       <div className="header-main-bottom">
         <span className="header-main-left">用户管理</span>
         <div className="header-main-right">
-          <span>{Date.now()}</span>
+          <span>{dayjs(sysTime).format('YYYY-MM-DD HH:mm:ss')}</span>
           <img src={logo} alt=""/>
           <span>晴</span>
         </div>
