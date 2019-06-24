c5988da7a5e74de0343fde9d697ec2a89b816819
ninglin00 <845893576@qq.com>
2019-06-24 15:32:56 +0800

    完成退出对话

--- src/components/right-header/index.jsx
+++ src/components/right-header/index.jsx
@@ -1,24 +1,39 @@
 
 // 右侧头部组件
 import React, { Component } from 'react';
+import { Modal } from 'antd';
 
 import ConmonButton from "../conmon-button";
-import { getItem } from '../../until/storage-tool'
+import { getItem, removeItem } from '../../until/storage-tool';
 
 import logo from '../../assets/images/logo.png'
 import './index.less'
 
+const { confirm } = Modal;
 
 export default class RightHeader extends Component {
 
   componentWillMount(){
     this.username = getItem().username
   }
+  logout = () => {
+    confirm({
+      title: '您确认要退出登录吗?',
+      okText: '确认',
+      cancelText: '取消',
+      onOk() {
+        //确认则清除数据
+        removeItem();
+        // 退出登录
+        this.props.history.replace('/login');
+      }
+    })
+  }
   render() {
     return <div>
       <div className="header-main-top">
         <span>欢迎, {this.username}</span>
-        <ConmonButton>退出</ConmonButton>
+        <ConmonButton onClick={this.logout}>退出</ConmonButton>
       </div>
       <div className="header-main-bottom">
         <span className="header-main-left">用户管理</span>
